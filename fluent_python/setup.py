d4018da7302091cb664615b8d38616b497682ee5
Sean Meling Murray <sean.meling@gmail.com>
2020-03-15 19:24:42 +0100

    ran black

--- fluent_python/setup.py
+++ fluent_python/setup.py
@@ -10,5 +10,5 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
